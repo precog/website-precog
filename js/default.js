a1e31698d60ce9e2cf422a7067734a8c7a64fb10
Matthew T De Goes <matthew@reportgrid.com>
2012-05-08 10:10:12 -0600

    Precog Site Update
    Style Change to developer page and modifications to the javascript for
    side menus. Change Sticker.

--- js/default.js
+++ js/default.js
@@ -407,6 +407,38 @@ $("#dev-home-link").click(function(){
     }
     );
 
+$(function() {
+    $.fn.scrollBottom = function() {
+        return $(document).height() - this.scrollTop() - this.height();
+    };
+
+    var $el = $('#content-right-menu');
+    var $window = $(window);
+
+    $window.bind("scroll resize", function() {
+        var gap = $window.height() - $el.height() - 10;
+        var visibleFoot = 172 - $window.scrollBottom();
+        var scrollTop = $window.scrollTop()
+
+        if(scrollTop < 172 + 10){
+            $el.css({
+                top: (172 - scrollTop) + "px",
+                bottom: "auto"
+            });
+        }else if (visibleFoot > gap) {
+            $el.css({
+                top: "auto",
+                bottom: visibleFoot + "px"
+            });
+        } else {
+            $el.css({
+                top: 0,
+                bottom: "auto"
+            });
+        }
+    });
+});
+
 //PRICING
 $(".body-pricing-difference").mouseover(function(){
       $(".body-pricing-difference-li").animate({
